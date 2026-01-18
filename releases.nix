a8650831d90b57ed88478aec6bad0d5e27fa3981
mangoiv <contact@mangoiv.com>
2026-01-18 11:51:45 +0100

    releases.nix: get rid of nixpkgs pin to 19.09

--- releases.nix
+++ releases.nix
@@ -1,15 +1,4 @@
-{ baseNixpkgs ? (import <nixpkgs> {}), system ? baseNixpkgs.system }:
-
-let
-  nixpkgsSrc = baseNixpkgs.fetchFromGitHub {
-    owner = "nixos";
-    repo = "nixpkgs";
-    rev = "c2ef0cee28a17f6fcb64ea0d0fb705f8c5ee6cf3";
-    sha256 = "05vvj1693p6d56l9wl7f2cxdrn57sdgx6wa1vph67brl8xzlmzbc";
-  };
-  nixpkgs = import nixpkgsSrc { inherit system; };
-in with nixpkgs;
-
+{ nixpkgs ? (import <nixpkgs> {}) }:
 let
   fromBindist = { url, sha256, ncursesVersion ? "6" }:
     nixpkgs.callPackage ./artifact.nix {} {
